84e6c425751f6705143f7428fee5843a4fe79219
Arjo <ac.ala.arya@gmail.com>
2021-07-06 19:24:58 +0530

    last add

--- code/Functions/countdown.hs
+++ code/Functions/countdown.hs
@@ -1,44 +1,47 @@
+-- Solving the Countdown Problem:
+
+-- Given, a :: [Int] and n :: Int find all possibilities
+-- such that we can form an expression with arithmetic
+-- operators (+, -, *, /) over a to yield n
+
+-- Formalize the problem
+
+-- Operation definiton
 data Op = Add | Sub | Mul | Div
 
-apply :: Op -> Int -> Int -> Int
-apply Add x y = x + y
-apply Sub x y = x - y
-apply Div x y = div x y
-apply Mul x y = x * y
-
+-- What is a valid operation?
 valid :: Op -> Int -> Int -> Bool
 valid Add _ _ = True
 valid Sub x y = x > y
 valid Mul _ _ = True
-valid Div x y = mod x y == 0
+valid Div x y = mod x y > 0
 
-data Expr = Val Int | App Op Expr Expr
+-- Application
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = div x y
 
-eval :: Expr -> [Int]
-eval (Val n) = [n | n > 0]
-eval (App o l r) = [apply o x y | x <- eval l, 
-                        y<- eval r, valid o x y]
+-- Expression definiton
+data Exp = Val Int | App Op Exp Exp
 
-values :: Expr -> [Int]
+values :: Exp -> [Int]
 values (Val n) = [n]
 values (App _ l r) = values l ++ values r
 
+eval :: Exp -> [Int]
+eval (Val n) = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l, 
+                                  y <- eval r, 
+                                  valid o x y]
+
+subbags :: [a] -> [[a]]
+subbags xs = [zs | ys <- subs xs, zs <- perms ys]
+
 solution :: Expr -> [Int] -> Int -> Bool
-solution e ns n = elem (values e) (choices ns) 
-                  && eval e == [n]
+solution :: e ns n = elem (values e) (subbags ns) && eval e == [n]
 
-exprs :: [Int] -> [Expr]
-exprs [] = []
-exprs [n] = [Val n]
-exprs ns = [e | (ls, rs) <- split ns,
-                l <- exprs ls,
-                r <- rs,
-                e <- combine l r]
+-- Implementation
 
-combine :: Expr -> Expr -> [Expr]
-combine l r = [App o l r | o <- [Add, Sub, Mul, Div]]
-
-solutions :: [Int] -> Int -> [Expr]
-solutions ns n = [e | ns' <- choices ns,
-                      e <- exprs ns',
-                      eval e == [n]]
+split :: [a] -> []
