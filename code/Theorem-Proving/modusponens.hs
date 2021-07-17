684889d4759a1533fb95dbc39cc230db92ec82b2
Arjo <ac.ala.arya@gmail.com>
2021-07-17 15:39:59 +0530

    haskell update

--- code/Theorem-Proving/modusponens.hs
+++ code/Theorem-Proving/modusponens.hs
@@ -1,16 +1,14 @@
 data Rule = MP | Assumption
--- ((A, B), C) is equiv to (A => B) => C
-
 data Term = Val String | Implies Term Term
-
 data Proof = Proof Term [Term] Rule [Proof]
 
--- A => B, A implies B       
--- B [A, (A, B)] MP []
+eval :: Term -> 
 
 exists :: Term -> [Term] -> Bool
-exists c assumptions = c `elem` assumptions
+exists 
 
-eval :: Proof -> Bool
-eval (Proof c assumptions Assumption proofs) = exists c assumptions
-eval _ = False
+-- Assumption Rule:
+-- Proof A [A, A => B] Assumption []
+-- eval :: Proof -> Bool
+-- eval (Proof c assumptions Assumption proofs) = exists c assumptions
+-- eval _ = False
